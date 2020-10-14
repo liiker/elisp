8d93bf97bd4e3a6b72f624678e6aec56e347eb9e
mac <mac@macdeMacBook-Pro.local>
2020-10-14 22:57:04 +0800

    update: init zzld-java package.

--- elisps/zzld-java.el
+++ elisps/zzld-java.el
@@ -1,1 +1,30 @@
+;;; zzld-java.el --- Emacs-lisp tips                    -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2020  Wentao Yang
+
+;; Author: Feng Shu <nx.adamyang@gmail.com>
+;; Keywords: java, devtools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+
+
+(provide 'zzld-java)
+;;; zzld-java.el ends here
